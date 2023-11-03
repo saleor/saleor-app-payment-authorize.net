ca73af6ba872c6c23e143116d12731c96fa0c5e9
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2023-11-03 09:59:47 +0100

    refactor: :recycle: change logs in webhook response

--- src/lib/webhook-response-builder.ts
+++ src/lib/webhook-response-builder.ts
@@ -12,12 +12,12 @@ export class SynchronousWebhookResponseBuilder<TWebhookName extends keyof SyncWe
   constructor(private res: NextApiResponse) {}
 
   ok(response: SyncWebhookResponse<TWebhookName>) {
-    this.logger.debug({ response }, "responding with");
+    this.logger.debug({ response }, "responding with JSON:");
     this.res.status(200).json(response);
   }
 
   internalServerError(error: Error) {
-    this.logger.error({ error }, "responding with");
+    this.logger.error({ error }, "responding with error:");
     this.res.status(500).json({
       error: {
         message: error.message,
