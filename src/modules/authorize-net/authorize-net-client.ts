f21ad940dc7189531f9a3549ee9f658003584416
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2023-11-13 11:54:05 +0100

    feat: :sparkles: add getHostedPaymentPageRequest method to client

--- src/modules/authorize-net/authorize-net-client.ts
+++ src/modules/authorize-net/authorize-net-client.ts
@@ -8,17 +8,19 @@ const ApiContracts = AuthorizeNet.APIContracts;
 const ApiControllers = AuthorizeNet.APIControllers;
 const SDKConstants = AuthorizeNet.Constants;
 
+const messagesSchema = z.object({
+  resultCode: z.string(),
+  message: z.array(
+    z.object({
+      code: z.string(),
+      text: z.string(),
+    }),
+  ),
+});
+
 // AuthorizeNet types don't contain the response
 const createTransactionResponseSchema = z.object({
-  messages: z.object({
-    resultCode: z.string(),
-    message: z.array(
-      z.object({
-        code: z.string(),
-        text: z.string(),
-      }),
-    ),
-  }),
+  messages: messagesSchema,
   transactionResponse: z
     .object({
       responseCode: z.string(),
@@ -60,8 +62,17 @@ const createTransactionResponseSchema = z.object({
 
 type CreateTransactionResponse = z.infer<typeof createTransactionResponseSchema>;
 
+const getHostedPaymentPageResponseSchema = z.object({
+  messages: messagesSchema,
+  token: z.string(),
+});
+
+type GetHostedPaymentPageResponse = z.infer<typeof getHostedPaymentPageResponseSchema>;
+
+type ResponseMessages = z.infer<typeof messagesSchema>;
+
 // todo: test
-function formatAuthorizeErrors(messages: CreateTransactionResponse["messages"]) {
+function formatAuthorizeErrors(messages: ResponseMessages) {
   return messages.message
     .map(({ code, text }) => {
       return `${code}: ${text}`;
@@ -75,12 +86,15 @@ export class AuthorizeNetClient {
     name: "AuthorizeNetClient",
   });
 
-  constructor(private config: AuthorizeProviderConfig.FullShape) {
+  config: AuthorizeProviderConfig.FullShape;
+
+  constructor(config: AuthorizeProviderConfig.FullShape) {
     const merchantAuthenticationType = new ApiContracts.MerchantAuthenticationType();
     merchantAuthenticationType.setName(config.apiLoginId);
     merchantAuthenticationType.setTransactionKey(config.transactionKey);
 
     this.merchantAuthenticationType = merchantAuthenticationType;
+    this.config = config;
   }
 
   private getEnvironment() {
@@ -112,6 +126,61 @@ export class AuthorizeNetClient {
           const apiResponse = transactionController.getResponse();
           const response = new ApiContracts.CreateTransactionResponse(apiResponse);
           const parsedResponse = createTransactionResponseSchema.parse(response);
+
+          if (parsedResponse.messages.resultCode === "Error") {
+            const message = formatAuthorizeErrors(parsedResponse.messages);
+
+            throw new AuthorizeNetError(message);
+          }
+
+          resolve(parsedResponse);
+        });
+      } catch (error) {
+        if (error instanceof z.ZodError) {
+          reject(error.format());
+        }
+        reject(error);
+      }
+    });
+  }
+
+  async getHostedPaymentPageRequest(
+    transactionInput: AuthorizeNet.APIContracts.TransactionRequestType,
+  ): Promise<GetHostedPaymentPageResponse> {
+    const createRequest = new ApiContracts.GetHostedPaymentPageRequest();
+    createRequest.setMerchantAuthentication(this.merchantAuthenticationType);
+    createRequest.setTransactionRequest(transactionInput);
+
+    // Authorize API needs at least one setting 🤷
+    const setting1 = new ApiContracts.SettingType();
+    setting1.setSettingName("hostedPaymentButtonOptions");
+    setting1.setSettingValue('{"text": "Pay"}');
+
+    const settingList = [];
+    settingList.push(setting1);
+
+    const alist = new ApiContracts.ArrayOfSetting();
+    alist.setSetting(settingList);
+    createRequest.setHostedPaymentSettings(alist);
+
+    const transactionController = new ApiControllers.GetHostedPaymentPageController(
+      createRequest.getJSON(),
+    );
+
+    transactionController.setEnvironment(this.getEnvironment());
+
+    return new Promise((resolve, reject) => {
+      try {
+        transactionController.execute(() => {
+          // eslint disabled because of insufficient types
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          const apiResponse = transactionController.getResponse();
+          const response = new ApiContracts.GetHostedPaymentPageResponse(apiResponse);
+
+          this.logger.debug({ response }, "getHostedPaymentPageRequest");
+          const parsedResponse = getHostedPaymentPageResponseSchema.parse(response);
+
+          this.logger.debug({ parsedResponse });
 
           if (parsedResponse.messages.resultCode === "Error") {
             const message = formatAuthorizeErrors(parsedResponse.messages);
