7d59d4d21f10fcd3a62f6d8911f1d2e72a82dc8b
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2023-11-28 14:34:59 +0100

    feat: :sparkles: add create-transaction client

---
+++ src/modules/authorize-net/client/create-transaction.ts
@@ -0,0 +1,48 @@
+import AuthorizeNet from "authorizenet";
+
+import { z } from "zod";
+import { AuthorizeNetClient, baseAuthorizeObjectSchema } from "./authorize-net-client";
+
+const ApiContracts = AuthorizeNet.APIContracts;
+const ApiControllers = AuthorizeNet.APIControllers;
+
+const createTransactionSchema = baseAuthorizeObjectSchema.and(z.unknown());
+
+type CreateTransactionResponse = z.infer<typeof createTransactionSchema>;
+
+export class CreateTransactionClient extends AuthorizeNetClient {
+  async execute(
+    transactionInput: AuthorizeNet.APIContracts.TransactionRequestType,
+  ): Promise<CreateTransactionResponse> {
+    const createRequest = new ApiContracts.CreateTransactionRequest();
+    createRequest.setMerchantAuthentication(this.merchantAuthenticationType);
+    createRequest.setTransactionRequest(transactionInput);
+
+    const transactionController = new ApiControllers.CreateTransactionController(
+      createRequest.getJSON(),
+    );
+
+    transactionController.setEnvironment(this.getEnvironment());
+
+    return new Promise((resolve, reject) => {
+      transactionController.execute(() => {
+        try {
+          // eslint disabled because of insufficient types
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          const apiResponse = transactionController.getResponse();
+          const response = new ApiContracts.CreateTransactionResponse(apiResponse);
+          const parsedResponse = createTransactionSchema.parse(response);
+
+          this.resolveResponseErrors(parsedResponse);
+
+          resolve(parsedResponse);
+        } catch (error) {
+          if (error instanceof z.ZodError) {
+            reject(error.format());
+          }
+          reject(error);
+        }
+      });
+    });
+  }
+}
